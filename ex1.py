7c402a6b46d73797566b3cbaf090a39a385c3424
sng <sng@bsdnetworks.net>
2018-03-25 18:31:13 +0000

    Mass add

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print "Hello world."
+print "Hello Again"
+print "I like typing this."
+print "Ansible!"
+print "Salt!"
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
