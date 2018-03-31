dabf54a486c601e98676285467062721f632fb1a
sng <sng@bsdnetworks.net>
2018-03-31 20:11:18 +0000

    ex24.py

---
+++ ex24.py
@@ -0,0 +1,22 @@
+print "Let's practice everything."
+print 'You\'d need to to know \'bout escapes with \\ that do \n newlines and \t tabs.'
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an eplanation
+\n\t\twhere there is none.
+"""
+
+print "------------------"
+print poem
+print "------------------"
+
+five = 10 - 2 + 3 - 6
+print "This should be five: %s" % five
+
+def secret_formula(started):
+    jelly_beans
+
