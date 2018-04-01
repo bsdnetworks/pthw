f936b25c1ac5c829aee25de977c5e62d5eac6ac0
sng <sng@bsdnetworks.net>
2018-04-01 15:59:37 +0000

    Fixed ex25.py

--- ex25.py
+++ ex25.py
@@ -1,6 +1,6 @@
 def break_words(stuff):
     """This function will break up words for us."""
-    words = stuff.split(' ')
+    words = stuff.split(' ' )
     return words
 
 def sort_words(words):
@@ -9,7 +9,7 @@ def sort_words(words):
 
 def print_first_word(words):
     """ Prints the first word after popping it off"""
-    word = word.pop(0)
+    word = words.pop(0)
     print word
 
 def print_last_word(words):
